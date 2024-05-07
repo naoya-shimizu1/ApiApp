a865ef92f3becd01ba6a0965defca0250c90f779
naoya.shimizu <Naoya_Shimizu@jrass.jp>
2024-05-07 15:15:52 +0900

    first commit

---
+++ ApiApp/FavoriteShop.swift
@@ -0,0 +1,25 @@
+//
+//  FavoriteShop.swift
+//  ApiApp
+//
+//  Created by PC-SYSKAI555 on 2024/04/30.
+//
+
+import RealmSwift
+
+class FavoriteShop: Object {
+    // 店舗id
+    @Persisted(primaryKey: true) var id = ""
+    
+    // 店舗名
+    @Persisted var name = ""
+    
+    // 店舗画像URL
+    @Persisted var logoImageURL = ""
+    
+    // 店舗住所
+    @Persisted var address = ""
+    
+    // クーポン画面URL
+    @Persisted var couponURL = ""
+}
---
+++ ApiApp/ShopCell.swift
@@ -0,0 +1,18 @@
+//
+//  ShopCell.swift
+//  ApiApp
+//
+//  Created by PC-SYSKAI555 on 2024/04/26.
+//
+
+import UIKit
+
+class ShopCell: UITableViewCell {
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var favoriteButton: UIButton!
+    @IBOutlet weak var shopNameLabel: UILabel!
+    @IBOutlet weak var logoImageView: UIImageView!
+    
+}
+
+        // Configure the view for the selected state
